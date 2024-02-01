236abf49aecaf66dabb420e9d6dc5f066bdcd51f
박수현 <soohyun.park@kt.com>
2024-02-01 17:47:41 +0900

    상품수정 API 개발 스프링부트 테스트로 전환하기

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -10,7 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.productorderservice.dto.AddProductRequest;
 import com.example.productorderservice.dto.GetProductResponse;
+import com.example.productorderservice.dto.UpdateProductRequest;
 import com.example.productorderservice.service.ProductService;
+import com.example.productorderservice.type.DiscountPolicy;
 
 @SpringBootTest
 class ProductServiceTest {
@@ -38,5 +40,22 @@ class ProductServiceTest {
 
 		// then - 상품 응답 검증
 		assertThat(response).isNotNull();
+	}
+
+	@Test
+	@Transactional
+	public void 상품수정() throws Exception {
+		// given - 상품 등록
+		productService.addProduct(ProductTestSteps.상품등록요청_생성());
+		final Long productId = 1L;
+
+		// when - 상품 업데이트
+		final UpdateProductRequest request = new UpdateProductRequest("상품명수정", 2000, DiscountPolicy.NONE);
+		productService.updateProduct(productId, request);
+
+		// then
+		final GetProductResponse response = productService.getProduct(productId);
+		assertThat(response.name()).isEqualTo(request.name());
+		assertThat(response.price()).isEqualTo(request.price());
 	}
 }
